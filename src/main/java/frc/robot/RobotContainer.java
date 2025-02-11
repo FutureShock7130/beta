649faff38ddbadffab6d6e88e59bbf041cc47530
shrubman8787 <138845668+shrubman8787@users.noreply.github.com>
2025-02-11 17:41:21 +0800

    added swerve code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,15 +1,26 @@
 package frc.robot;
 
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.DriveCommands;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Grabber;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.SuperStruct;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.GyroIOPigeon2;
+import frc.robot.subsystems.drive.ModuleIO;
+import frc.robot.subsystems.drive.Swerve;
 public class RobotContainer {
     
     private CommandXboxController m_Controller;
@@ -20,20 +31,90 @@ public class RobotContainer {
     private final Intake m_intake;
     private final SuperStruct m_SuperStruct;
 
+    
+  
+  private final Drive drive;
+  private final Vision vision = new Vision();
+
+  private final CommandXboxController controller = new CommandXboxController(0);
+
+  
+  private final SendableChooser<Command> autoChooser;
+
     public RobotContainer() {
         m_elevator = Elevator.getInstance();
         m_grabber = Grabber.getInstance();
         m_intake = Intake.getInstance();
         m_SuperStruct = SuperStruct.getInstance();
+
+            switch (Constants.currentMode) {
+      case REAL:
+        // Real robot, instantiate hardware IO implementations
+
+        drive =
+            new Drive(
+                new GyroIOPigeon2(),
+                new Swerve(0),
+                new Swerve(1),
+                new Swerve(2),
+                new Swerve(3),
+                vision);
+
+        break;
+
+      // case SIM:
+      //   // Sim robot, instantiate physics sim IO implementations
+      //   drive =
+      //       new Drive(
+      //           new GyroIO() {},
+      //           new ModuleIOSim(),
+      //           new ModuleIOSim(),
+      //           new ModuleIOSim(),
+      //           new ModuleIOSim());
+      //   break;
+
+      default:
+        // Replayed robot, disable IO implementations
+        drive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                vision);
+        break;
+    }
+
+    // Set up auto routines
+
+    autoChooser = AutoBuilder.buildAutoChooser();
+
         
         configureButtonBindings();
     }
 
     private void configureButtonBindings() {
+            drive.setDefaultCommand(
+        DriveCommands.joystickDrive(
+            drive,
+            () -> -controller.getLeftY(),
+            () -> -controller.getLeftX(),
+            () -> -controller.getRightX()));
+    controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
+    controller
+        .b()
+        .onTrue(
+            Commands.runOnce(
+                    () ->
+                        drive.setPose(
+                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
+                    drive)
+                .ignoringDisable(true));
     }
 
     public Command getAutonomousCommand() {
-        return null;
+        return autoChooser.getSelected();
     }
 
     
---
+++ src/main/java/frc/robot/subsystems/drive/ModuleIO.java
@@ -0,0 +1,47 @@
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import org.littletonrobotics.junction.AutoLog;
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+public interface ModuleIO {
+  @AutoLog
+  public static class ModuleIOInputs implements LoggableInputs {
+    public double drivePositionRad = 0.0;
+    public double driveVelocityRadPerSec = 0.0;
+    public double driveAppliedVolts = 0.0;
+    public double[] driveCurrentAmps = new double[] {};
+
+    public Rotation2d turnAbsolutePosition = new Rotation2d();
+    public Rotation2d turnPosition = new Rotation2d();
+    public double turnVelocityRadPerSec = 0.0;
+    public double turnAppliedVolts = 0.0;
+    public double[] turnCurrentAmps = new double[] {};
+    @Override
+    public void toLog(LogTable table) {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'toLog'");
+    }
+    @Override
+    public void fromLog(LogTable table) {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'fromLog'");
+    }
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(ModuleIOInputs inputs) {}
+
+  /** Run the drive motor at the specified voltage. */
+  public default void setDriveVoltage(double volts) {}
+
+  /** Run the turn motor at the specified voltage. */
+  public default void setTurnVoltage(double volts) {}
+
+  /** Enable or disable brake mode on the drive motor. */
+  public default void setDriveBrakeMode(boolean enable) {}
+
+  /** Enable or disable brake mode on the turn motor. */
+  public default void setTurnBrakeMode(boolean enable) {}
+}
